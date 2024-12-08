df3389e9654765ac0175bb94e728d15705bd924c
davletmegahorosh <davletazimov142@gmail.com>
2024-12-08 15:27:33 +0600

    settings

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = "django-insecure-gb&t%6od1jeeyy^=e9-5ha)8syp2syve$!nx7ez9r@vhg=nk3%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
